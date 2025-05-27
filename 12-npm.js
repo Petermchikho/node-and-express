6b99bb7e94ef087715469a7850630700724fe60f
Petermchikho <petercharlesmchikho1@gmail.com>
2025-05-27 16:01:57 +0200

    npm review

--- 12-npm.js
+++ 12-npm.js
@@ -2,6 +2,8 @@
 // npm init
 // npm init -y
 // npm install lodash
+// npm install nodemon -D
+// npm install -g nodemon 
 
 const _ = require('lodash')
 
--- app.js
+++ app.js
@@ -6,4 +6,4 @@ if (amount<10){
     console.log('large number')
 }
 
-console.log(`hello world`)
+console.log(`hello world o`)
